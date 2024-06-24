02a58feb23e1e89d25df0d256023c102e8ae438c
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-06-24 12:15:00 -0300

    1

---
+++ src/components/Landing/Info.jsx
@@ -0,0 +1,128 @@
+import React, { useRef, useEffect } from "react"
+import styled, { keyframes } from "styled-components"
+
+const Container = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 20px;
+`
+
+const Content = styled.div`
+  position: relative;
+  z-index: 1;
+  width: 70%;
+    background: rgb(179 179 179 / 28%);
+  border-radius: 20px;
+  padding: 20px;
+  backdrop-filter: blur(30px);
+`
+
+const moveAndShapeAnimation = keyframes`
+  0%, 100% {
+    transform: translate(0, 0) scale(1);
+    border-radius: 50%;
+  }
+  25% {
+    transform: translate(100px, -50px) scale(1.2);
+    border-radius: 50%;
+  }
+  50% {
+    transform: translate(-50px, 50px) scale(1.4);
+    border-radius: 40%;
+  }
+  75% {
+    transform: translate(-100px, 0) scale(1.1);
+    border-radius: 50%;
+  }
+`
+
+const RedBall = styled.div`
+  position: absolute;
+  top: 100px; 
+  left: calc(50% - 50px); 
+  width: 100px;
+  height: 100px; 
+  background: rgb(253, 29, 29);
+  background: linear-gradient(
+    90deg,
+    rgba(253, 29, 29, 1) 0%,
+    rgba(255, 0, 0, 1) 46%,
+    rgba(252, 176, 69, 1) 100%
+  );
+  border-radius: 50%; 
+  z-index: 0; 
+  animation: ${moveAndShapeAnimation} 15s ease-in-out infinite; 
+`
+
+const Title = styled.h1`
+  margin-bottom: 10px;
+ 
+`
+
+const Boxes = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+`
+
+const Box = styled.div` 
+    display: flex;
+    flex-direction: column;
+    width: 200px;
+`
+
+const SubTitle = styled.h1`
+   background: rgb(253,29,29);
+  background: linear-gradient(90deg, rgba(253,29,29,1) 0%, rgba(255,0,0,1) 46%, rgba(252,176,69,1) 100%);
+  -webkit-background-clip: text;
+  color: transparent;
+  font-size: 1.5rem;
+  font-weight: bold;
+`
+
+const Text = styled.p`
+    font-size: 1.2rem;
+    font-weight: bold;
+     color: gray;
+`
+
+const Info = () => {
+  return (
+    <>
+      <Container>
+        <Content>
+          <Title>Info the new Crypto Games THESA</Title>
+          
+            <Boxes>
+                <Box>
+                    <SubTitle>100M</SubTitle>
+                    <Text>
+                        Max suplly is 100M Tokens
+                    </Text>
+                </Box>
+                <Box>
+                    <SubTitle>10K Users</SubTitle>
+                    <Text>
+                        News Users every days
+                    </Text>
+                </Box>
+                <Box>
+                    <SubTitle>+20 Games</SubTitle>
+                    <Text>
+                       The Best games
+                    </Text>
+                </Box>
+            </Boxes>
+        </Content>
+       
+        <RedBall />
+      </Container>
+    </>
+  );
+};
+
+export default Info;
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react"
 import Navbar from "../Header/Navbar"
 import styled from "styled-components"
+import Info from "./Info"
 
 const Container = styled.div`
   background-color: ${(props) =>
@@ -8,28 +9,91 @@ const Container = styled.div`
   color: ${(props) => (props.theme.mode === 'dark' ? 'white' : 'black')};
 `
 
-const Title = styled.h1`
+const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 600px;
+`
 
+const Title = styled.h1`
+    font-size: 3.3rem;
+    font-weight: bold;
+    cursor: pointer;
+    margin: 10px;
+
+    @media(max-width: 768px) {
+    font-size: 2.5rem;
+    }
 `
 
 const Text = styled.p`
+    font-size: 0.9rem;
+    color: gray;
+    font-weight: bold;
+    cursor: pointer;
 
+    & span{
+        background: rgb(253,29,29);
+        background: linear-gradient(90deg, rgba(253,29,29,1) 0%, rgba(255,0,0,1) 46%, rgba(252,176,69,1) 100%);
+        -webkit-background-clip: text;
+        color: transparent;
+    }
+
+    @media(max-width: 768px) {
+      font-size: 0.85rem;
+    }
 `
 
 const SubText = styled.p`
+  font-size: 1rem;
+  color: gray;
+  font-weight: 400;
+  cursor: pointer;
 
+   @media(max-width: 768px) {
+      text-align: justify;
+      padding: 0 50px;
+    }
 `
 
 const Btns = styled.div`
-
+    display: flex;
+    gap: 20px;
+    padding: 40px 0px;
 `
 
 const BtnBuy = styled.button`
+  background: linear-gradient(90deg, rgb(227 227 227) 0%, rgb(147 147 147) 50%, rgb(55 55 55) 100%);
+  padding: 12px 18px;
+  text-decoration: none;
+  color: ${(props) => (props.theme.mode === 'dark' ? 'white' : 'black')};
+  font-weight: bold;
+  font-size: 1rem;
+  border-radius: 8px;
+  border: none;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2); /* Adiciona sombra ao fundo */
 
+  &:hover {
+    background: linear-gradient(90deg, rgba(150, 150, 150, 1) 0%, rgba(192, 192, 192, 1) 50%, rgba(0, 0, 0, 1) 100%);
+  }
 `
 
 const BtnGames = styled.button`
+ background: rgb(253,29,29);
+  background: linear-gradient(90deg, rgba(253,29,29,1) 0%, rgba(255,0,0,1) 46%, rgba(252,176,69,1) 100%);
+  padding: 12px 18px;
+  text-decoration: none;
+  color: ${(props) => (props.theme.mode === 'dark' ? 'white' : 'black')};
+  font-weight: bold;
+  font-size: 1rem;
+  border-radius: 8px;
+  border: none;
 
+  &:hover {
+    background: darkred;
+  }
 `
 
 const Landing = () => {
@@ -37,17 +101,22 @@ const Landing = () => {
   return (
     <Container >
       <Navbar />
-      <Text>Discovery the new era for Games  |  <span>Explore </span></Text>
-      <Title>Level up your game Expericience</Title>
-      <SubText></SubText>
-      <Btns>       
-        <BtnGames>
-          Play
-        </BtnGames>
-        <BtnBuy>
-          Buy
-        </BtnBuy>
-      </Btns>
+      <Content>
+        <Text>DISCOVERY THE NEW ERA FOR GAMES | <span> EXPLORE </span></Text>
+        <Title>Level up your <br></br> gaming Expericience</Title>
+        <SubText>Explore the decentralized gaming platform where fun and innovation converge,
+           with unique opportunities to earn tokens</SubText>
+        <Btns>       
+          <BtnGames>
+            Play
+          </BtnGames>
+          <BtnBuy>
+            Buy
+          </BtnBuy>
+        </Btns>
+      </Content>
+      <Info />
+      
     </Container>
   )
 }
