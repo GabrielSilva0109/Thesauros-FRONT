aeaefc7b4bb8df2a174f90e8c2a12df013d21eb0
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-06-26 11:35:47 -0300

    1

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+  background-color: ${(props) =>
+    props.theme.mode === 'dark' ? 'black' : 'white'};
+  color: ${(props) => (props.theme.mode === 'dark' ? 'white' : 'black')};
+`
+
+const Footer = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Footer
