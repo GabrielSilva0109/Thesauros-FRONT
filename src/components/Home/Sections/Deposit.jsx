9e93c9881f7085db7638408a6013ddf61dedb8f7
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-07-01 13:14:52 -0300

    1

--- src/components/Home/Sections/Deposit.jsx
+++ src/components/Home/Sections/Deposit.jsx
@@ -1,10 +1,53 @@
 import React from 'react'
 import { Button, Info, InfoItem, Input, Label } from '../Home'
+import styled from 'styled-components'
 
+import pix from '../../../IMG/logos/pix.png'
+import mastercard from '../../../IMG/logos/mastercard.png'
+import metamask from '../../../IMG/logos/metamask.png'
+
+const Content = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+`
+
+const Item = styled.div`
+    width: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center; 
+`
+
+const Img = styled.img`
+    width: 40px;
+`
+
+const MastercardImg = styled(Img)`
+    width: 60px;
+`
+
+const Separator = styled.div`
+    border-left: 1px solid black;
+`
 
 const Deposit = () => {
     return (
         <>
+        <Content>
+            <Item>
+                <Img src={pix} />
+            </Item>
+            <Item>
+                <MastercardImg src={mastercard} alt="MasterCard" />
+            </Item>
+            <Separator />
+            <Item>
+                <Img src={metamask} />
+            </Item>
+
+        </Content>
         <Info>
             <InfoItem>
                 <Label>Value</Label>
